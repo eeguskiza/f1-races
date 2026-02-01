cc85a04b21f674459b02b5473411ce2f809319c0
eeguskiza <e.eguskiza@opendeusto.es>
2026-02-01 15:38:56 +0100

    Arranques

--- predictions/views.py
+++ predictions/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.shortcuts import render, redirect
 
-# Create your views here.
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
+def home(request):
+    return render(request, "predictions/home.html")
+
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("predictions:home")
+    else:
+        form = SignupForm()
+    return render(request, "registration/signup.html", {"form": form})
